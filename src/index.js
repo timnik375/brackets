1f91d32729c7e17dc45f2ddc44348086af7512a2
timnik375 <67319575+timnik375@users.noreply.github.com>
2021-03-16 00:39:16 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i = 0; i < str.length + 3; i++) {
+        let temp;
+
+        bracketsConfig.forEach((elem) => {
+            temp = elem[0] + elem[1]
+            while (str.indexOf(temp) > -1) {
+                str = str.replace(`${temp}`, '')
+            }
+        })
+    }
+
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false
+    }
 }
